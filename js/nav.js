c690e5fd936e5bd9daed0fe550b52b1a6d9b9055
Rob Tarr <rob@robtarr.net>
2012-07-07 16:22:29 -0400

    Added JS Docs and SCSS. Reorganized some files, updated tests

--- js/nav.js
+++ js/nav.js
@@ -1,45 +1,74 @@
-var APP = {
-  getState: function() {
-    return window.getComputedStyle( document.getElementById( "sizeTest" ), ":after" ).getPropertyValue( "content" ) || "small";
-  }
-};
-
+/**
+ The NAV object houses all of the functionality for the main navigation
+*/
 var NAV = {
   
+  /**
+   ## clear
+   Remove any styles left over from JavaScript based animations
+  */
   clear: function() {
     NAV.$mainMenu.add( NAV.$mainMenu.find( "ul" ) ).removeAttr( "style" );
   },
 
+  /**
+   ## slideSubNav
+   Toggle the subNav menus open and closed states
+
+   @param e - event that triggered this method
+  */
   slideSubNav: function( e ) {
     e.preventDefault();
 
     if ( APP.getState() === "small" ) {
-      console.log( "slideSubNav" );
-      $( this ).siblings( "ul" ).stop().slideToggle().toggleClass( "open" );
+      $( this ).siblings( "ul" ).stop().slideToggle( "fast" );
     }
   },
 
+  /**
+   ## toggleNav
+   Toggle the display of the main nav. This is used at smaller sizes
+   because the nav is hidden by default for this context.
+  */
   toggleNav: function() {
     NAV.$mainMenu.toggle();
   },
-    
+  
+  /**
+   ## init
+   Setup the appropriate event handlers for the navigation
+  */  
   init: function() {
-    // cache this for later use
+    /**
+     Cache this for later use
+    */
     NAV.$mainMenu = $( "#mainMenu" );
 
-    // Show/Hide the main menu for smaller screens
+    /**
+     Show/Hide the main menu for smaller screens
+    */
     $( "#showNav" ).on( "click", NAV.toggleNav );
     
-    // Hover handler for first level nav
-    NAV.$mainMenu.find( ".subMenu" ).siblings( "a" ).on( "mouseover mouseout", NAV.slideSubNav );
+    /**
+     Click handler for first level nav
+    */
+    NAV.$mainMenu.find( ".subMenu" ).siblings( "a" ).on( "click", NAV.slideSubNav );
 
+    /**
+     When the window changes to greater than 410px, we need to clear out any
+     inline styles applied via JavaScript animations.
+    */
     mediaCheck({
-      media: '(min-width: 500px)',
+      media: '(min-width: 410px)',
       entry: NAV.clear
     });
   }
 };
 
+/**
+ ## on DOM Ready
+ Run the NAV init method
+*/
 $(function() {
   NAV.init();
 });
--- test/spec/NavSpec.js
+++ test/spec/NavSpec.js
@@ -1,27 +1,41 @@
 describe( "Nav", function() {
   
-  describe( "submenus", function() {
+  describe( "submenus when in the small context", function() {
+    
     var $subMenu;
     var $menuLink;
 
+    /**
+      Load in the some HTML to simulate the navigation markup
+    */
     loadFixtures( "fixtures/nav.html" );
 
+    /**
+      Fake the response from APP.getState() to return small so that we can test
+      the functionality for small sizes, independent of the current size of the
+      browser.
+    */
     APP = window.APP || {};
     APP.getState = function() { return "small"; };
-    NAV.init();
 
+    /**
+      Get a hook to the nav item that the test will "click" on.
+    */
     $subMenu = $( ".subMenu:first" );
+
+    /**
+      Get a hook to the subNav that the test will b elooking for.
+    */
     $menuLink = $subMenu.siblings( "a" );
 
-    it( "should slide the sub navs open on a mouseover when in the small context", function() {
-      console.log( "Running test" );
-      $menuLink.trigger( "mouseover" );
-      expect( $subMenu.hasClass( "open" ) ).toBe( true );
+    it( "should slide the sub navs open on the first click ", function() {
+      $menuLink.trigger( "click" );
+      expect( $subMenu.is( ":visible" ) ).toBe( true );
     });
 
-    // it( "should slide sub navs closed on a mouseout when in the small context", function() {
-    //   // $menuLink.trigger( "mouseout" );
-    //   expect( $subMenu.hasClass( "open" ) ).toBe( false );
-    // });
+    it( "should slide sub navs closed on the second click", function() {
+      $menuLink.trigger( "click" );
+      expect( $subMenu.is( ":visible" ) ).toBe( false );
+    });
   });
 });
